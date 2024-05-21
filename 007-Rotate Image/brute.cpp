43480427f4c4dd469ee49577bc9a4fb2536d54a9
Sweta Raj <80194324+Sweta-Raj31@users.noreply.github.com>
2024-05-21 20:46:48 +0530

    Update brute.cpp

--- 007-Rotate Image/brute.cpp
+++ 007-Rotate Image/brute.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // one solution is i will store a single data to its absolute position
+        vector<vector<int>>ans;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans[i][j]=matrix[j][n-1-i];
+            }
+        }
+        return ans;
+        
+    }
+};
+// time complexity is O(n^2)
+//space Complexity is O(N^2)
